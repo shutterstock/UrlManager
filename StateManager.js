43d38c027bc3a9c083d03d01d295c440ce49a104
Chris Becker <chrisb@shutterstock.com>
2013-07-23 15:08:03 -0400

    initial commit

---
+++ StateManager.js
@@ -0,0 +1,103 @@
+// manage reading and writing page state
+StateManager = function() {
+
+	var self = this;
+	self.state = {};
+	
+	self.setState = function(field,value) {
+		if (field.match(/\[\]$/)) {
+			if (!self.state[field]) {
+				self.state[field] = {};
+			}
+			self.state[field][value] = 1;
+		} else {
+			self.state[field] = value;
+		}
+		self.updateUrl();
+	}
+
+	self.removeState = function(field,value) {
+		if (field.match(/\[\]$/)) {
+			if (self.state[field] && self.state[field][value]) {
+				delete self.state[field][value];
+			}
+			
+		} else {
+			delete self.state[field];
+		}
+		self.updateUrl();
+	}	    
+
+	self.clearState = function(field) {
+		delete self.state[field];
+		self.updateUrl();
+	}
+
+	self.getStates = function() {
+		return self.state;
+	}
+
+	// get a specific field
+	self.getState = function(field) {
+		if (field.match(/\[\]$/)) {
+			var list = [];
+			// return objects as a list
+			if (self.state[field]) {
+				for (key in self.state[field]) {
+					list.push(key);
+				}
+				return list;
+			}
+		} else {
+			return self.state[field];
+		}
+	}
+
+	self.readUrl = function() {
+		var vars = {}, hash;
+		var q = document.URL.split('?')[1]
+
+		if(q != undefined && q.length){
+			q = q.replace(/#$/,'');
+			q = q.split('&');
+			for(var i = 0; i < q.length; i++){
+				pair = q[i].split('=');
+				var key = pair[0];
+				var val = decodeURIComponent(pair[1]);
+				if (typeof(key) === 'undefined' || typeof(val) === 'undefined') {
+					continue;
+				}
+				if (key.match(/\[\]$/)) {
+					if (!vars[key]) {
+						vars[key] = {};
+					}
+					vars[key][val] = 1;
+				} else {
+					vars[key] = val;
+				}
+			}
+
+			self.state = vars;
+		}
+	}
+
+	self.updateUrl = function() {
+		var params = [];
+
+		for (var field in self.state) {
+
+			if (Object.prototype.toString.call(self.state[field]) == '[object Object]') {
+				for (var value in self.state[field]) {
+					params.push(field+'='+encodeURIComponent(value));
+				}
+			} else {
+				params.push(field+'='+encodeURIComponent(self.state[field]));
+			}
+		}
+
+		var queryString = params.join('&');
+		history.pushState(null, queryString, '?' + queryString);
+	}
+
+	return self;
+}
